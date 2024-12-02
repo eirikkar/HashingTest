b0077677128142dd73bcd033c42e460419f4950a
eirikkar <eirikkar@proton.me>
2024-12-02 13:01:52 +0100

    Add hashing instance and test to main

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+      Hashing hashing = new Hashing();
+      string password = "password123";
+      string hash = hashing.Hash(password);
+      Console.WriteLine(hash);
     }
 }
