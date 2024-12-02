2b5ff34d555e63334166894647c8228f9ce24012
eirikkar <eirikkar@proton.me>
2024-12-02 12:57:45 +0100

    Add Hash method and Hashing class

---
+++ Classes/Hashing.cs
@@ -0,0 +1,14 @@
+using System.Security.Cryptography;
+public class Hashing
+{
+  private int SaltSize = 16;
+  private int HashSize = 32;
+  private int Iterations = 100000;
+  private static readonly HashAlgorithmName HashAlgorithm = HashAlgorithmName.SHA512;
+  public string Hash(string password)
+  {
+    byte[] salt = RandomNumberGenerator.GetBytes(SaltSize);
+    byte[] hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, HashAlgorithm, HashSize);
+    return $"{Convert.ToHexString(salt)}.{Convert.ToHexString(hash)}";
+  }
+}
