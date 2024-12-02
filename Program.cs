b560cbe703fe6cafb1161d3bb96b65a369cc8657
eirikkar <eirikkar@proton.me>
2024-12-02 13:02:57 +0100

    Add hashing without salt test to main

--- Program.cs
+++ Program.cs
@@ -8,5 +8,13 @@ class Program
       string password = "password123";
       string hash = hashing.Hash(password);
       Console.WriteLine(hash);
+
+      string password2 = "password123";
+      string hash2 = hashing.HashWithoutSalt(password2);
+      Console.WriteLine(hash2);
+
+      string password3 = "password123";
+      string hash3 = hashing.HashWithoutSalt(password3);
+      Console.WriteLine(hash3);
     }
 }
