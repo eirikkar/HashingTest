e688e7b0cf2582c4f03f4fb9e38a3c8c30edf785
eirikkar <eirikkar@proton.me>
2024-12-03 10:38:15 +0100

    Changed indeting

--- Classes/Hashing.cs
+++ Classes/Hashing.cs
@@ -1,34 +1,34 @@
 using System.Security.Cryptography;
 public class Hashing
 {
-  private int SaltSize = 16;
-  private int HashSize = 32;
-  private int Iterations = 100000;
-  private static readonly HashAlgorithmName HashAlgorithm = HashAlgorithmName.SHA512;
-  public string Hash(string password)
-  {
-    byte[] salt = RandomNumberGenerator.GetBytes(SaltSize);
-    byte[] hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, HashAlgorithm, HashSize);
-    return $"{Convert.ToHexString(salt)}.{Convert.ToHexString(hash)}";
-  }
-  public string HashWithoutSalt(string password)
-  {
-    byte[] hash = Rfc2898DeriveBytes.Pbkdf2(password, new byte[0], Iterations, HashAlgorithm, HashSize);
-    return Convert.ToHexString(hash);
-  }
+    private int SaltSize = 16;
+    private int HashSize = 32;
+    private int Iterations = 100000;
+    private static readonly HashAlgorithmName HashAlgorithm = HashAlgorithmName.SHA512;
+    public string Hash(string password)
+    {
+        byte[] salt = RandomNumberGenerator.GetBytes(SaltSize);
+        byte[] hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, HashAlgorithm, HashSize);
+        return $"{Convert.ToHexString(salt)}.{Convert.ToHexString(hash)}";
+    }
+    public string HashWithoutSalt(string password)
+    {
+        byte[] hash = Rfc2898DeriveBytes.Pbkdf2(password, new byte[0], Iterations, HashAlgorithm, HashSize);
+        return Convert.ToHexString(hash);
+    }
 
-  public bool Verify(string password, string hash)
-  {
-    string[] parts = hash.Split('.');
-    byte[] salt = Convert.FromHexString(parts[0]);
-    byte[] hashToCheck = Convert.FromHexString(parts[1]);
-    byte[] inputHash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, HashAlgorithm, HashSize);
-    return CryptographicOperations.FixedTimeEquals(inputHash, hashToCheck);
-  }
-  public bool VerifyWithoutSalt(string password, string hash)
-  {
-    byte[] hashToCheck = Convert.FromHexString(hash);
-    byte[] inputHash = Rfc2898DeriveBytes.Pbkdf2(password, new byte[0], Iterations, HashAlgorithm, HashSize);
-    return CryptographicOperations.FixedTimeEquals(inputHash, hashToCheck);
-  }
+    public bool Verify(string password, string hash)
+    {
+        string[] parts = hash.Split('.');
+        byte[] salt = Convert.FromHexString(parts[0]);
+        byte[] hashToCheck = Convert.FromHexString(parts[1]);
+        byte[] inputHash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, HashAlgorithm, HashSize);
+        return CryptographicOperations.FixedTimeEquals(inputHash, hashToCheck);
+    }
+    public bool VerifyWithoutSalt(string password, string hash)
+    {
+        byte[] hashToCheck = Convert.FromHexString(hash);
+        byte[] inputHash = Rfc2898DeriveBytes.Pbkdf2(password, new byte[0], Iterations, HashAlgorithm, HashSize);
+        return CryptographicOperations.FixedTimeEquals(inputHash, hashToCheck);
+    }
 }
